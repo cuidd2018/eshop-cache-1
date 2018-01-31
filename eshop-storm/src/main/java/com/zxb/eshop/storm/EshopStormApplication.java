fe61b9d9234df249474bc46174c2ed94d5ca02a7
xuery <zhuxuebin@sf-express.com>
2018-01-31 17:25:26 +0800

    eshop-storm init

---
+++ eshop-storm/src/main/java/com/zxb/eshop/storm/EshopStormApplication.java
@@ -0,0 +1,12 @@
+package com.zxb.eshop.storm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopStormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopStormApplication.class, args);
+	}
+}
